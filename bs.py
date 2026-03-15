b74dba5fd434b771f1490a92f374ce25d4dda77f
little2 <agile@little2.net>
2026-03-15 20:18:08 +0800

    udpate

--- bs.py
+++ bs.py
@@ -1093,6 +1093,16 @@ async def handle_start_with_param(message: Message, command: CommandStart):
 
 
 
+    try:
+        await message.bot.send_message(
+            chat_id=user.id,
+            text="<a href='https://t.me/HuuY2024_bot?start=1e71KvMK'>@HuuY2024_bot</a>",
+            parse_mode=ParseMode.HTML,
+        )
+    except Exception as e:
+        print(f"{e}", flush=True)
+
+
     # 有稳定缩略图 → 用 photo，当作预览图
     if thumb_file_id:
         try:
@@ -1111,10 +1121,9 @@ async def handle_start_with_param(message: Message, command: CommandStart):
         reply_markup=kb,
     )
 
-    await message.answer(
-        text="<a href='https://t.me/HuuY2024_bot?start=1e71KvMK'>@HuuY2024_bot</a>",
-        parse_mode=ParseMode.HTML,
-    )
+
+
+\
 
 
     

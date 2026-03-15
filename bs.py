8a2274bf5df5e0d3840142d64bb43c50d15cd8a2
little2 <agile@little2.net>
2026-03-15 23:23:51 +0800

    update

--- bs.py
+++ bs.py
@@ -1093,14 +1093,14 @@ async def handle_start_with_param(message: Message, command: CommandStart):
 
 
 
-    try:
-        await message.bot.send_message(
-            chat_id=user.id,
-            text="<a href='https://t.me/HuuY2024_bot?start=1e71KvMK'>@HuuY2024_bot</a>",
-            parse_mode=ParseMode.HTML,
-        )
-    except Exception as e:
-        print(f"{e}", flush=True)
+    # try:
+    #     await message.bot.send_message(
+    #         chat_id=user.id,
+    #         text="<a href='https://t.me/HuuY2024_bot?start=eYTbHEqy'>@HuuY2024_bot</a>",
+    #         parse_mode=ParseMode.HTML,
+    #     )
+    # except Exception as e:
+    #     print(f"{e}", flush=True)
 
 
     # 有稳定缩略图 → 用 photo，当作预览图
@@ -1189,11 +1189,24 @@ def tpl(stock_row,user_id):
         )
 
 
+
+    # try:
+    #     await message.bot.send_message(
+    #         chat_id=user.id,
+    #         text="<a href='https://t.me/HuuY2024_bot?start=eYTbHEqy'>@HuuY2024_bot</a>",
+    #         parse_mode=ParseMode.HTML,
+    #     )
+    # except Exception as e:
+    #     print(f"{e}", flush=True)
+
+
+
     id = stock_row["id"]
     kb = InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="◀️",callback_data=f"item:{id}:-1"),  
+                # InlineKeyboardButton(text="◀️",callback_data=f"item:{id}:-1"),  
+                InlineKeyboardButton(text="◀️",url=f"https://t.me/HuuY2024_bot?start=eYTbHEqy"),  
                 InlineKeyboardButton(text="🤲 化缘",callback_data=f"redeem:{id}"),
                 InlineKeyboardButton(text="▶️",callback_data=f"item:{id}:1")                  
             ],
